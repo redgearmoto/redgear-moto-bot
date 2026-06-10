6e3adfd463680b7d947a39f6a58930542e7268e4
redgearmoto <redgear.moto1@gmail.com>
2026-06-10 02:27:03 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -164,15 +164,18 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text("Введи витрату:\n\n250 масло Motul")
         return
 
-    if text == "💼 Баланс":income, expense, balance = 
-        await get_balance()
-        await update.message.reply_text(
-            f"💼 Баланс RedGear Moto\n\n"
-            f"Доходи: {income:.2f} zł\n"
-            f"Витрати: {expense:.2f} zł\n"
-            f"Баланс: {balance:.2f} zł"
-        )
-        return
+    if text == "💼 Баланс":
+
+    income, expense, balance = await get_balance()
+
+    await update.message.reply_text(
+        f"💼 Баланс RedGear Moto\n\n"
+        f"Доходи: {income:.2f} zł\n"
+        f"Витрати: {expense:.2f} zł\n"
+        f"Баланс: {balance:.2f} zł"
+    )
+
+    return
 
     if text == "➕ Новий клієнт":
         context.user_data["mode"] = "new_client"
